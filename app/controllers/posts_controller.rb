2843c57042c9b969267fe28a9ac2bdf49ec6e359
Charles Clemens <cclemensjr@gmail.com>
2017-04-15 21:06:23 -0700

    Add new and create actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,13 @@ class PostsController < ApplicationController
   def index
   end
 
+  def new
+    @post = Post.new
+  end
+
   def create
+    @post = Post.create(post_params)
+    redirect_to posts_path
   end
 
   def read
@@ -13,4 +19,12 @@ class PostsController < ApplicationController
 
   def delete
   end
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :caption)
+  end
+  
 end
