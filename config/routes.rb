087b10b88b6141a6cf1fe3570ed0009d64c1388f
Charles Clemens <cclemensjr@gmail.com>
2017-04-18 13:20:19 -0700

    Add ability for logged in users to create new posts and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
-  resources :posts
+  resources  :posts
 
-  root 'posts#index'
+  root       'posts#index'
 end
