7a116ab2e9fa258e96fc6c0fd1b6e2d86a46a66e
Charles Clemens <cclemensjr@gmail.com>
2017-04-19 04:08:44 -0700

    Create the create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,26 @@
 class CommentsController < ApplicationController
+  before_action :set_post
+
+  def create
+    @comment = @post.comments.build(comment_params)
+    @comment.user_id = current_user.id
+
+    if @comment.save
+      flash[:success] = "You commented successfully!"
+      redirect_to :back
+    else
+      flash[:alert] = "Your comment was thwarted. Please check the comment form."
+      render root_path
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comments).permit(:content)
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
 end
