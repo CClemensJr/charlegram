12ff9f00eb9b7eb8cfb719ad9796a420d393c69e
Charles Clemens <cclemensjr@gmail.com>
2017-04-20 07:54:11 -0700

    Tidy up

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
 
   has_attached_file                 :image, styles: { medium: "640px" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  validates_length_of               :caption, minimum: 3, maximum: 300, allow_blank: true
 
   belongs_to :user
   has_many :comments, dependent: :destroy
